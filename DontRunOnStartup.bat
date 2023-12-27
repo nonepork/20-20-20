4518b7fe48c3c923fd3975c15fafcec5d7ff0932
nonepork <59335048+nonepork@users.noreply.github.com>
2023-12-27 13:38:33 +0800

    Delete DontRunOnStartup.bat

--- DontRunOnStartup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-del "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\TwentyTwentyTwenty.lnk" && echo Success! This app shouldn't run on startup now
-
-timeout /t 5
