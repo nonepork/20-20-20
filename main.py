f04cb5f697ac1c02ea0a82071c4d1e9c6d432eba
nonepork <59335048+nonepork@users.noreply.github.com>
2023-12-27 14:32:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import sys
 import os
 from datetime import datetime
+from VividHues import Clr
 
 pygame.font.init()
 pygame.mixer.init()
@@ -16,6 +17,7 @@ SMALL_FONT = pygame.font.SysFont('comicsans', 100)
 BIG_FONT = pygame.font.SysFont('comicsans', 250)
 LIGHT_GRAY = (200, 200, 200)
 DARK_GRAY = (30, 30, 30)
+first_time = True
 
 def draw(timer, timerPos):
     WIN.fill(DARK_GRAY)
@@ -62,28 +64,32 @@ def main(timerValue, font):
 
 
 if __name__ == "__main__":
+    os.system('cls')
+    print(Clr.BOLD + Clr.random("スタート~") + Clr.RS)
     while True:
-        WIN = pygame.display.set_mode((150, 150), pygame.NOFRAME)
-        HALF_WIDTH, HALF_HEIGHT = 75, 75
+        if not first_time:
+            WIN = pygame.display.set_mode((150, 150), pygame.NOFRAME)
+            HALF_WIDTH, HALF_HEIGHT = 75, 75
 
-        win32gui.SetWindowPos(pygame.display.get_wm_info()['window'], win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
+            win32gui.SetWindowPos(pygame.display.get_wm_info()['window'], win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
 
-        if not main(10, SMALL_FONT):
-            time.sleep(1200)
-            continue
+            if not main(10, SMALL_FONT):
+                time.sleep(1200)
+                continue
 
-        WIN = pygame.display.set_mode((0, 0))
-        WIDTH, HEIGHT = WIN.get_size()
-        HALF_WIDTH, HALF_HEIGHT = WIDTH/2, HEIGHT/2
+            WIN = pygame.display.set_mode((0, 0))
+            WIDTH, HEIGHT = WIN.get_size()
+            HALF_WIDTH, HALF_HEIGHT = WIDTH/2, HEIGHT/2
 
-        win32gui.SetWindowPos(pygame.display.get_wm_info()['window'], win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
+            win32gui.SetWindowPos(pygame.display.get_wm_info()['window'], win32con.HWND_TOPMOST, 0, 0, 0, 0, win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
 
-        main(20, BIG_FONT)
+            main(20, BIG_FONT)
+        else:
+            first_time = False
 
-        # records when 20 min ups
+        # Record current time when times up
         now = datetime.now()
-
         current_time = now.strftime("%H:%M:%S")
-        print("Current Time =", current_time)
+        print("Current Time =", Clr.jazzy(current_time) + Clr.RS)
 
         time.sleep(1200)
