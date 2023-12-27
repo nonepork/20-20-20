a1cea17d42c5d0d2ddc6f2c879bcb1598ea32a48
nonepork <59335048+nonepork@users.noreply.github.com>
2023-12-27 13:38:39 +0800

    Delete RunOnStartup.bat

--- RunOnStartup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Start Menu\Programs\Startup\TwentyTwentyTwenty.lnk');$s.TargetPath='%~dp0/TwentyTwentyTwenty.exe';$s.Save()" && echo Success! This app should run on startup now
-
-timeout /t 5
